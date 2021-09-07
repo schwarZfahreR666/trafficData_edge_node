eb54a4d3eaddae63d129820e9194f44f761fab58
Zhang Ran <qqq190979389@163.com>
2021-09-07 15:59:25 +0800

    添加消息队列拒绝

--- src/main/java/com/example/edge_node/rabbitmq/AutoCloudEdgeConsumer.java
+++ src/main/java/com/example/edge_node/rabbitmq/AutoCloudEdgeConsumer.java
@@ -2,11 +2,16 @@ package com.example.edge_node.rabbitmq;
 
 import com.alibaba.fastjson.JSONObject;
 import com.example.edge_node.service.TaskService;
+import com.rabbitmq.client.Channel;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.support.AmqpHeaders;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
 
 /**
  * Create by zhangran
@@ -19,18 +24,27 @@ public class AutoCloudEdgeConsumer {
     @Autowired
     TaskService taskService;
     @RabbitListener(queues = {"auto-cloud-edge-queue"})
-    public void reviceMessage(String message){
+    public void reviceMessage(String message, @Header(AmqpHeaders.DELIVERY_TAG) long deliveryTag, Channel channel){
         JSONObject jsonObject =  JSONObject.parseObject(message);
         log.info(">>>>>>>>>> record =" + jsonObject.toJSONString());
         String taskName = jsonObject.getString("task_name");
         String input = "";
         String nodeName = jsonObject.getString("node_name");
         String res = "no";
+        Status status = new Status(-1,"启动任务失败",taskName);
         if("BUAA".equals(nodeName)){
-                    String ans = taskService.startTask(taskName,input,res);
+            String ans = taskService.startTask(taskName,input,res);
+            status = new Status(0,"启动任务成功",taskName);
+        }
+        else{
+            try {
+                channel.basicReject(deliveryTag,true);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
 
-        Status status = new Status(0,"启动任务成功",taskName);
+
         String exchangeName = "auto-edge-cloud";
         String routingKey = "";
         edgeCloudProducer.send(exchangeName,routingKey,status);
